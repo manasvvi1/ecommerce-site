08b087e466ab1893c749a0afce2a17cc6b65a7ae
Manasvvi <manasvvi.aggarwal@gmail.com>
2021-12-06 20:52:16 +0530

    trending women clothes page added

---
+++ scripts/cards.js
@@ -0,0 +1,20 @@
+var parent = document.getElementById("productCards");
+
+var images = ["https://image.made-in-china.com/202f0j00oOStnyLzMgcF/Modern-Fashion-Elegant-Women-Clothes.webp", "https://cdn.shopify.com/s/files/1/2059/2623/products/ICHOIX-women-clothes-2018-fashion-autumn-winter-dress-sexy-party-dress-elegant-tube-long-sleeve-ladies_74172ea0-d43f-48a2-86c7-836d6119cfb4_800x.jpg"];
+
+var links = ["www.google.com", "www.facebook.com"];
+
+// images.forEach(index =>{
+//     parent.innerHTML += "<div class=\"col\"><a href=" + links[index] + "class=\"linkText\"><img src=" + images[index] + "><div class=\"card-body\"><h5 class=\"card-title\">Handmade Paper bags</h5></a></div>";
+// })
+
+// /* <div class="col"> -->
+//           <!-- <div class="card h-100">
+//             <a href="www.google.com" class="linkText">
+//             <img src="https://5.imimg.com/data5/SELLER/Default/2020/12/BK/FZ/KI/3080253/brown-paper-bag-500x500.jpg"  height="300" width="100" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">Handmade paper bags</h5>
+//               <p class="card-text">Manufactured by XYZ company. HURRY before they are sold out!!</p>
+//             </div>
+//         </a>
+//           </div> --> */
